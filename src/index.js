e66111e361cc2f481a3c756efc74366e75a22c6b
DanikKul <92942037+DanikKul@users.noreply.github.com>
2022-01-08 14:25:59 +0300

    fix: remove statement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = function toReadable (number) {
-  module.exports = function toReadable (number) {
     let dictOnes = {0 : "zero",1 : "one", 2 : "two", 3 : "three", 4 : "four", 5 : "five", 6 : "six", 7 : "seven", 8 : "eight", 9 : "nine" };
     let dictTens = {1 : "ten", 2 : "twenty", 3 : "thirty", 4 : "forty", 5 : "fifty", 6 : "sixty", 7 : "seventy", 8 : "eighty", 9 : "ninety"};
     let dictEl = {0 : "ten", 1 : "eleven", 2 : "twelve", 3 : "thirteen", 4 : "fourteen", 5 : "fifteen", 6 : "sixteen", 7 : "seventeen", 8 : "eighteen", 9 : "nineteen"};
@@ -47,4 +46,4 @@ module.exports = function toReadable (number) {
     }
     return result;
 }
-}
+
